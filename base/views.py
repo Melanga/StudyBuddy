032ab8c511404534a7d7cbdd3a14c8827d500941
Melanga Dissanayake <dissanayakedmmb@gmail.com>
2022-03-09 21:35:54 +0530

    rendered relational data in the template.

--- base/views.py
+++ base/views.py
@@ -1,17 +1,14 @@
 from django.shortcuts import render
-
-rooms = [
-    {'id': 1, 'name': 'lets learn python'},
-    {'id': 2, 'name': 'Design with me'},
-    {'id': 3, 'name': 'Frontend developers'}
-]
+from .models import Room
 
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 
 def room(request, pk):
-    context = {'room': i for i in rooms if i['id'] == int(pk)}
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
     return render(request, 'base/room.html', context)
