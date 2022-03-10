b0bc1c5dedc6e26d90dbdfbaad0815815af9e1c7
Melanga Dissanayake <dissanayakedmmb@gmail.com>
2022-03-10 13:28:20 +0530

    filter by topic is added

--- base/views.py
+++ base/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
 
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms': rooms}
+    # get q value in url to filter out the topic
+    q = request.GET.get('q') if request.GET.get('q') is not None else ''
+    rooms = Room.objects.filter(topic__name__icontains=q)
+    topics = Topic.objects.all()
+    context = {'rooms': rooms, 'topics': topics}
     return render(request, 'base/home.html', context)
 
 
