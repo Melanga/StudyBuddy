cfbef5090ed9ac2805445ceeb0194b3214d31aac
Melanga Dissanayake <dissanayakedmmb@gmail.com>
2022-03-11 11:27:12 +0530

    logout functionality is added

--- base/views.py
+++ base/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.db.models import Q
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -25,6 +25,11 @@ def login_page(request):
 
     context = {}
     return render(request, 'base/login_register.html', context)
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('home')
 
 
 def home(request):
