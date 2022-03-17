3b325b7f4d9b710621b3c0054cd41c9c71835bac
Melanga Dissanayake <dissanayakedmmb@gmail.com>
2022-03-17 13:37:03 +0530

    user editing functionality is added with theme

--- base/views.py
+++ base/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic, Message
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 
 
 def login_page(request):
@@ -53,6 +53,18 @@ def register_user(request):
         else:
             messages.error(request, 'An error occurred during registration')
     return render(request, 'base/login_register.html', {'form': form})
+
+
+@login_required(login_url='login')
+def update_user(request):
+    selected_user = request.user
+    form = UserForm(instance=selected_user)
+    if request.method == "POST":
+        form = UserForm(request.POST, instance=selected_user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk=selected_user.id)
+    return render(request, 'base/update_user.html', {'form': form})
 
 
 def home(request):
